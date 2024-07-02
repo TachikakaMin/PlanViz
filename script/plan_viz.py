ef195a02bd8b439fb1b1423960a6bf3a156d5e85
TachikakaMin <tangym@shanghaitech.edu.cn>
2024-07-02 02:32:34 -0400

    add more colors

--- script/plan_viz.py
+++ script/plan_viz.py
@@ -596,7 +596,7 @@ class PlanViz:
                 self.pcf.canvas.move(agent.agent_obj.obj, cur_move[0], cur_move[1])
                 self.pcf.canvas.move(agent.agent_obj.text, cur_move[0], cur_move[1])
                 self.pcf.canvas.move(agent.dir_obj, cur_move[0], cur_move[1])
-                colors = {0:"#54A1D4", 1:"#ED7668", 2:"#ED7668", 3:"#DBE769", 4:"#DBE769"}
+                colors = {0:"#54A1D4", 1:"#ED7668", 2:"#ED7668", 3:"#DBE769", 4:"#DBE769", 5:"#DBE769", 6:"#DBE769"}
                 color = colors[agent.path[next_tstep[ag_id]][3]]
                 self.pcf.canvas.itemconfig(agent.agent_obj.obj, fill=color)
                 # self.pcf.canvas.move(agent.dir_obj, _rad_ * _cos, _rad_ * _sin)
@@ -668,7 +668,7 @@ class PlanViz:
                 self.pcf.canvas.move(agent.agent_obj.obj, cur_move[0], cur_move[1])
                 self.pcf.canvas.move(agent.agent_obj.text, cur_move[0], cur_move[1])
                 self.pcf.canvas.move(agent.dir_obj, cur_move[0], cur_move[1])
-                colors = {0:"#54A1D4", 1:"#ED7668", 2:"#ED7668", 3:"#DBE769", 4:"#DBE769"}
+                colors = {0:"#54A1D4", 1:"#ED7668", 2:"#ED7668", 3:"#DBE769", 4:"#DBE769", 5:"#DBE769", 6:"#DBE769"}
                 color = colors[agent.path[next_tstep[ag_id]][3]]
                 self.pcf.canvas.itemconfig(agent.agent_obj.obj, fill=color)
                 # self.pcf.canvas.move(agent.dir_obj, _rad_ * _cos, _rad_ * _sin)
